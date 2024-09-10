2cf0efd646f962f0b6534388c0dcf0abc1609dbf
Emerson <emersonjuniormt@gmail.com>
2024-09-09 22:52:48 -0300

    Example Lambdas.java

---
+++ src/Lambdas.java
@@ -0,0 +1,20 @@
+public class Lambdas {
+
+    interface FactorialNumber{
+        boolean test(int a , int b);
+    }
+
+
+    public static void main(String[] args) {
+
+        FactorialNumber isFactor = (a,b) -> (b % a) == 0;
+
+        if(isFactor.test(9, 5)){
+            System.out.println("5 is a factor of 9");
+        }else {
+            System.out.println("5 is not a factor of 9 ");
+        }
+
+
+    }
+}
